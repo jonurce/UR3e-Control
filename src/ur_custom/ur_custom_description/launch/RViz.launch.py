0b0c861710074074c893530a6857386ab54f0ae1
jonurce <jonur@stud.ntnu.no>
2025-05-10 11:33:48 +0200

    Gazebo removed

---
+++ src/ur_custom/ur_custom_description/launch/RViz.launch.py
@@ -0,0 +1,57 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+def generate_launch_description():
+    # Get package directory
+    pkg_share = get_package_share_directory('ur_custom_description')
+
+    # Declare launch argument for the URDF/Xacro file
+    model_arg = DeclareLaunchArgument(
+        name='model',
+        default_value=os.path.join(pkg_share, 'urdf', 'ur3e_onrobot_2fg7_urdf.xacro'),
+        description='Path to robot URDF/Xacro file'
+    )
+
+    # Process Xacro file to URDF, explicitly as a string
+    robot_description = ParameterValue(
+        Command(['xacro ', LaunchConfiguration('model')]),
+        value_type=str
+    )
+    # Nodes
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    joint_state_publisher = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher'
+    )
+
+    joint_state_publisher_gui = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(pkg_share, 'rviz', 'urdf.rviz')]
+    )
+
+    return LaunchDescription([
+        model_arg,
+        robot_state_publisher,
+        joint_state_publisher_gui,
+        rviz_node
+    ])
